66c4b3a04b28a9204e423792309c1b0101435d5f
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-09-25 13:52:45 +0100

    environmental setup

---
+++ src/Components/Task.jsx
@@ -0,0 +1,11 @@
+const TaskManager = () =>{
+    return(
+        <div className="tasks">
+            <div className="headings">
+                <span>Hi!, It's a beautiful day</span>
+                <span>Add Todo</span>
+                <span>Have a good one</span>
+            </div>
+        </div>
+    )
+}
