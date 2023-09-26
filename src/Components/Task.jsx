d1fdfb26f2b6d2252ea2ea2ef34d0310a66d66fc
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-09-26 15:15:01 +0100

    functionalities added

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,8 +1,37 @@
+import React, { useState, useEffect } from "react";
 import { GiCancel } from "react-icons/gi";
 import { GiCheckMark } from "react-icons/gi";
 import { IconContext } from "react-icons";
 
 const TaskManager = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTasks = () =>{
+//Get task name and due date
+    const taskName = document.getElementById("taskName").value;
+    const dueDate = document.getElementById("dueDate").value;
+
+    //craete new task
+
+    const newTask = {name: taskName, dueDate};
+
+    //Add new task to the tasks state
+    setTasks([...tasks, newTask]);
+
+    // Clear the input fields
+    document.getElementById("taskName").value = "";
+    document.getElementById("dueDate").value = "";
+
+  }
+
+  //Delete todos
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+
   return (
     <div className="tasks">
       <div className="image-">
@@ -11,69 +40,37 @@ const TaskManager = () => {
             <span>Hi!, It's a beautiful day</span>
           </div>
           <div className="AddTask">
-            <input type="text" required placeholder="Write a todo" />
+            <input type="text" id="taskName" required placeholder="Write a todo" />
             <label>Due Date</label>
-            <input type="date" id="start" name="trip-start" />
-            <button>Add Todo</button>
+            <input type="date" id="dueDate" name="trip-start" />
+            <button onClick={addTasks}>Add Todo</button>
           </div>
         </div>
       </div>
       <div className="OutputTodo">
         <ul>
-          <li>
-            <span>
-              <p>Structure the business plan project to perfection</p>
-                  </span>
+          {/* Map over the tasks and display them */}
+          {tasks.map((task, index) => (
+            <li key={index}
+            className={task.completed ? "completed" : ""}>
+              <span>
+                <p>{task.name}</p>
+              </span>
               <div className="flexy">
-              <h6>9/25/2023</h6>
-            <div className="indicator">
-              <IconContext.Provider value={{ className: "top-react-icons" }}>
-                <span>
-                  <GiCancel className="cancel" />
-                </span>
-                <span>
-                  <GiCheckMark className="complete" />
-                </span>
-              </IconContext.Provider>
-            </div>
-            </div>
-          </li>
-          <li>
-            <span>
-              <p>Structure the business plan project to perfection</p>
-                  </span>
-              <div className="flexy">
-              <h6>9/25/2023</h6>
-            <div className="indicator">
-              <IconContext.Provider value={{ className: "top-react-icons" }}>
-                <span>
-                  <GiCancel className="cancel" />
-                </span>
-                <span>
-                  <GiCheckMark className="complete" />
-                </span>
-              </IconContext.Provider>
-            </div>
-            </div>
-          </li>
-          <li>
-            <span>
-              <p>Structure the business plan project to perfection</p>
-                  </span>
-              <div className="flexy">
-              <h6>9/25/2023</h6>
-            <div className="indicator">
-              <IconContext.Provider value={{ className: "top-react-icons" }}>
-                <span>
-                  <GiCancel className="cancel" />
-                </span>
-                <span>
-                  <GiCheckMark className="complete" />
-                </span>
-              </IconContext.Provider>
-            </div>
-            </div>
-          </li>
+                <h6>{task.dueDate}</h6>
+                <div className="indicator">
+                  <IconContext.Provider value={{ className: "top-react-icons" }}>
+                    <span>
+                      <GiCancel className="cancel"  onClick={() => deleteTask(index)}/>
+                    </span>
+                    <span>
+                      <GiCheckMark className="complete"/>
+                    </span>
+                  </IconContext.Provider>
+                </div>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
